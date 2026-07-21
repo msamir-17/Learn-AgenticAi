cc272b616c44269a6259ec42a074c2d0beb8f562
Samir <mksamir013@gmail.com>
2026-07-22 01:05:27 +0530

    Theory Of Project done Now starting the implementation

--- 3_conditional_wf.py
+++ 3_conditional_wf.py
@@ -32,3 +32,22 @@
 # Instead of "add_edges " (which always goestothe same node) we used "Add_Conditional_edges" (which calls the router function to decide).
 # eg = builder.add_edges("A","B")
 #    = builder.add_conditional_edges("A",router) // router is a function
+
+# The Router Function can make decision in two ways:
+#  1. Rule-Based (py logic) :
+# we write the rules bu ourself. simple predictable, fast
+
+#  2. LLM-based (Ai Decides)
+# we ask the LLM to classify or decide, and route based on its answer. more flexible, handles ambiguity, "but slower" 
+
+# PROJECT : COLLEGE CHATBOT
+
+# we have 3 branches in our clg BCA , BCOM , BBA
+
+# A student will choose one of these 3 options and then a chatbot will be activated.we can ask question to that chatbot, but the llm you are usingin that chatbot does not have knowledge of the college programme. for that we will have 2 PDFs the frist one will be for academics and the second one willbe for fee-related things. 
+
+# 3 conditional path
+#     1. Answer the question using the academic PDF (using RAG)
+#     2. Answer the question using the fee PDF (using RAG)
+#     3. Answer General question based on the LLMsown Knowledge
+# And every conditional paths response converges to a single node 
